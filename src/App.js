a109296c59205a21ec20f5f09c035180a080362a
Lavnish <lavnishdesigner@gmail.com>
2023-08-25 23:08:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Chaudhary
               Family</span>
           </a>
-          <form className='order-3 col-span-2 md:col-span-1 md:order-none'>
+          {/* <form className='order-3 col-span-2 md:col-span-1 md:order-none'>
             <label htmlFor="default-search"
               className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative my-2">
@@ -102,7 +102,8 @@ function App() {
                 <span className="sr-only">Search</span>
               </button>
             </div>
-          </form>
+          </form> */}
+          <div></div>
           <div className='flex justify-end md:hidden'>
             <button data-collapse-toggle="navbar-default" type="button"
               className="inline-flex justify-end items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -117,7 +118,7 @@ function App() {
             </button>
           </div>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul
+            {/* <ul
               className="font-medium flex flex-col justify-end p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a href="#"
@@ -128,11 +129,7 @@ function App() {
                 <a href="#"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
               </li>
-              <li>
-                <a href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Search</a>
-              </li>
-            </ul>
+            </ul> */}
           </div>
         </div>
       </nav>
